e8f1827e960e01a242a3ac8f7eaca9806c3626f3
caspian <caspianmaclean@gmail.com>
2021-02-07 12:59:26 +0800

    finding the answer

--- puzzle-2020-1a/solve.py
+++ puzzle-2020-1a/solve.py
@@ -2,6 +2,24 @@
 inputfile = open("input.txt")
 lines=inputfile.readlines()
 
-#test file reading has worked
-print(lines[1])
-print(len(lines))
+#length of list
+n=len(lines)
+
+#convert to array of numbers
+numbers=[]
+for i in range(n):
+    numbers.append(int(lines[i]))
+
+#test converted numbers
+#print(numbers[0]+numbers[199])
+
+for i in numbers:
+    #print(i)
+    for j in numbers:
+        if i+j == 2020:
+            print(i)
+            print(j)
+            print(i*j)
+ 
+#prints answer twice, once with the first number as i, once with the first number as j
+#answer is: 1926*94, or 181044
